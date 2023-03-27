0df1b9fe1d6ca0d5e51de0f4c88b2ee4c16d73f0
Ljnelski <Ljnelski@gmail.com>
2023-03-27 19:32:53 -0400

    Part 16

---
+++ EverythingElse/VulkanPart16/VulkanPart16/lve_model.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "lve_device.hpp"
+
+// libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace lve
+{
+	class LveModel
+	{
+	public:
+
+		// ALWAYS UPDATE BINDING ATTRIBUTIONS IF CHANGING THE MODEL STRUCTURE
+		struct Vertex
+		{
+			glm::vec3 position;
+			glm::vec3 color;
+
+			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+		};
+
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		LveModel(LveDevice& device, const LveModel::Builder &builder);
+		~LveModel();
+
+		LveModel(const LveModel &) = delete;
+		LveModel &operator=(const LveModel&) = delete;
+
+		void bind(VkCommandBuffer commandBuffer);
+		void draw(VkCommandBuffer commandBuffer);
+	private:
+		void createVertexBuffers(const std::vector<Vertex>& vertices);
+		void createIndexBuffers(const std::vector<uint32_t>& indices);
+
+		LveDevice lveDevice;
+
+		VkBuffer vertexBuffer;
+		VkDeviceMemory vertexBufferMemory;
+		uint32_t vertexCount;
+
+		bool hasIndexBuffer = false;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
+		uint32_t indexCount;
+	};
+}
